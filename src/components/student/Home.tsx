8569f85278053aa7c7ab3e4bbe19206223d3547b
haderhameedpc-hue <hader.hameed.pc@gmail.com>
2026-09-24 22:27:09 +0300

    Updated Home.tsx

--- src/components/student/Home.tsx
+++ src/components/student/Home.tsx
@@ -98,13 +98,7 @@ export function Home({ student, onLogout, onOpenSettings, onSelectQuiz }: HomePr
             </div>
           </div>
           <div className="flex items-center gap-0">
-            <a
-              href="#admin"
-              className="btn-ghost "
-              title="لوحة الإدارة"
-            >
-              <Shield className="w-5 h-5" />
-            </a>
+            
             <ThemeToggle />
             <button onClick={onOpenSettings} className="btn-ghost" title="الإعدادات">
               <Settings className="w-5 h-5" />
