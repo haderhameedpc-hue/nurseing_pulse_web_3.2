ee41bc2d43104bfe2fbe2e6e0b547b12a2dc56f1
haderhameedpc-hue <hader.hameed.pc@gmail.com>
2026-09-24 22:40:59 +0300

    Updated Home.tsx

--- src/components/student/Home.tsx
+++ src/components/student/Home.tsx
@@ -112,7 +112,7 @@ export function Home({ student, onLogout, onOpenSettings, onSelectQuiz }: HomePr
             
             <a
               href="#admin"
-              className="btn-ghost justify-center p-2 rounded-lg text-slate-600"
+              className="btn-ghost justify-center p-2 "
               title="لوحة الإدارة"
             >
               <Shield className="w-5 h-5" />
