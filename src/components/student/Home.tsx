3c6360a8bc7c1d52a7fc4ed3a9386e93fcf6a61a
haderhameedpc-hue <hader.hameed.pc@gmail.com>
2026-09-24 22:52:39 +0300

    Updated Home.tsx

--- src/components/student/Home.tsx
+++ src/components/student/Home.tsx
@@ -111,8 +111,7 @@ export function Home({ student, onLogout, onOpenSettings, onSelectQuiz }: HomePr
 </a>
             
            
-              <Shield className="w-5 h-5" />
-            </a>
+             
             <ThemeToggle />
             <button onClick={onOpenSettings} className="btn-ghost justify-center p-2 hover:text-sky-500 dark:hover:text-sky-400 hover:bg-slate-100 dark:hover:bg-slate-800 transition-colors" title="الإعدادات">
               <Settings className="w-5 h-5" />
